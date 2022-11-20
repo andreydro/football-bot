85b2014f1e8c7a2fabe8fe3cd5681be57ace6f4d
andreydro <andreydro14@gmail.com>
2022-11-20 15:13:44 +0100

    Generate matches and participants for active admin

---
+++ app/admin/matches.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Match do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :title, :location, :number_of_players, :user_id, :have_ball_and_shirtfronts, :start, :duration, :finish
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:title, :location, :number_of_players, :user_id, :have_ball_and_shirtfronts, :start, :duration, :finish]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
---
+++ app/admin/participants.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Participant do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :match_id, :user_id, :aasm_state, :additional
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:match_id, :user_id, :aasm_state, :additional]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
