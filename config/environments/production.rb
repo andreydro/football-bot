fd42c057a532b8f1e5e2b820691c1e5f0255b2b4
andreydro <andreydro14@gmail.com>
2022-11-21 18:25:07 +0100

    Add locale for production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.hosts.clear
+  config.i18n.available_locales = %i[be en]
+  config.i18n.default_locale = :be
 end
