bc739cc29421232aeb84857b7a990398e715b30b
andreydro <andreydro14@gmail.com>
2022-11-27 14:04:27 +0100

    Send noticiation only to active matches

--- app/services/matches_notifications.rb
+++ app/services/matches_notifications.rb
@@ -8,7 +8,7 @@ class MatchesNotifications
   end
 
   def send_reminder(hours)
-    future_matches = Match.where(start: current_time..current_time + hours.hours)
+    future_matches = Match.active.where(start: current_time..current_time + hours.hours)
     text = I18n.t('match.begin_in_notification', hours: hours)
 
     future_matches.each do |match|
@@ -19,7 +19,7 @@ class MatchesNotifications
   end
 
   def send_morning_reminder
-    future_matches = Match.where(start: current_time.beginning_of_day..current_time.end_of_day)
+    future_matches = Match.active.where(start: current_time.beginning_of_day..current_time.end_of_day)
     text = I18n.t('match.morning_notification')
 
     future_matches.each do |match|
