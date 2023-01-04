df040673e3d08c64acac6417456595a5c650cf47
andreydro <andreydro14@gmail.com>
2023-01-04 20:46:12 +0100

    Resolve 2 bugs, match leaving and duplicate in match joining

--- app/services/match_service.rb
+++ app/services/match_service.rb
@@ -83,16 +83,22 @@ class MatchService
     user = User.find_by(telegram_id: message.from.id)
     match ||= Match.find_by(id: match_id)
     participant = Helpers.participant_object(user, match, false)
+    participant_already_joined = match.participants
+                                      .find_by(user_id: user.id, match_id: match.id, additional: false)
+                                      .present?
 
-    if participant.save
-      text = Helpers.match_info_text(match)
-      markup = Helpers.markup_object([Helpers.view_all_matches_button,
-                                      Helpers.add_plus_one_button(match_id),
-                                      Helpers.cant_come_button(participant)])
-
-      Helpers.send_message(client, message, text, markup)
+    if participant_already_joined
+      Helpers.send_message(client, message, I18n.t('match.already_participating_in_the_match'))
     else
-      Helpers.send_message(client, message, I18n.t('match.error_joining_match'))
+      if participant.save
+        text = Helpers.match_info_text(match)
+        markup = Helpers.markup_object([Helpers.view_all_matches_button,
+                                        Helpers.add_plus_one_button(match_id),
+                                        Helpers.cant_come_button(participant)])
+        Helpers.send_message(client, message, text, markup)
+      else
+        Helpers.send_message(client, message, I18n.t('match.error_joining_match'))
+      end
     end
   end
 
@@ -152,15 +158,20 @@ class MatchService
   def self.cant_come(client, message)
     participant_id = message.data.match(/\d+/)[0]
     participant = Participant.find_by(id: participant_id)
+    match ||= participant.match
+    leaving_times_is_over = match.start > 3.hours.ago
 
-    if participant.go_to_wont_come!
-      match ||= participant.match
-      ParticipantsService.new.update_participants_list(match)
-      markup = Helpers.markup_object([Helpers.show_match_button(match),
-                                      Helpers.view_all_matches_button])
-      Helpers.send_message(client, message, "#{I18n.t('match.status_change_message')} #{match.title}", markup)
+    if leaving_times_is_over
+      Helpers.send_message(client, message, I18n.t('match.cant_leave_match_before_start'))
     else
-      Helpers.send_message(client, message, I18n.t('match.error_changing_status'))
+      if participant.go_to_wont_come!
+        ParticipantsService.new.update_participants_list(match)
+        markup = Helpers.markup_object([Helpers.show_match_button(match),
+                                        Helpers.view_all_matches_button])
+        Helpers.send_message(client, message, "#{I18n.t('match.status_change_message')} #{match.title}", markup)
+      else
+        Helpers.send_message(client, message, I18n.t('match.error_changing_status'))
+      end
     end
   end
 end
