dd4334519b63953ba9f8fdb8f65800e10b2c3c15
andreydro <andreydro14@gmail.com>
2023-01-22 17:52:58 +0100

    Add spec when user exists

--- spec/services/users/register_spec.rb
+++ spec/services/users/register_spec.rb
@@ -3,24 +3,32 @@
 require 'rails_helper'
 
 RSpec.describe Users::Register do
+  let(:client) { nil }
+  let(:message) do
+    MessageHelpers::Message.new(1_111_111, 'username', 'first_name', 'last_name')
+  end
+
+  subject { Users::Register.new(client, message) }
+
+  before do
+    allow(Helpers).to receive(:send_message).and_return('message send')
+  end
+
   context 'user does not exist' do
-    let(:client) { nil }
-    let(:message) do
-      MessageHelpers::Message.new(1_111_111, 'username', 'first_name', 'last_name')
-    end
-
-    subject { Users::Register.new(client, message) }
-
-    before do
-      allow(Helpers).to receive(:send_message).and_return('message send')
-    end
-
     it 'creates user' do
       expect(subject.call).to eq 'message send'
       expect(User.first.username).to eq message.from.username
     end
   end
 
-  # context 'user exists' do
-  # end
+  context 'user exists' do
+    before do
+      create(:user, telegram_id: 1_111_111)
+    end
+
+    it ' does not create user' do
+      expect(subject.call).to eq 'message send'
+      expect(User.count).to eq 1
+    end
+  end
 end
